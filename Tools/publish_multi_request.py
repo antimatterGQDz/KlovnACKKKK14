ab63ba3c3fa53be5cebb41b0a098cb5056af396b
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-03-30 00:54:46 +0800

    ez

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://game.klovnstation.org/cdn/"
+ROBUST_CDN_URL = "http://game.klovnstation.org:9090/cdn/"
 FORK_ID = "ks14"
 
 def main():
